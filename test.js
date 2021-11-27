93df2901473d8bef89664490295c69adacd48d41
Davisa94 <Austin.i.davis94@gmail.com>
2021-11-26 21:45:06 -0800

    Added functionality for multiple media types

--- test.js
+++ test.js
@@ -1,1 +1,4 @@
-var latest = "changing some text";
+var media = "https://c.tenor.com/OZy3cP4bAvkAAAAS/revenge-ninja-peek-a-boo.gif";
+var memeImage = "https://c.tenor.com/OZy3cP4bAvkAAAAS/revenge-ninja-peek-a-boo.gif";
+var topText = "Top Text goes Here";
+var bottomText = "Bottom Text goes Here testing the overflow of longer text";
