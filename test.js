2b7e4bb229c5efe9322c7de4e7c37c12f5dde3f1
Davisa94 <Austin.i.davis94@gmail.com>
2021-11-26 22:16:56 -0800

    Begin modifying readme instructions

--- test.js
+++ test.js
@@ -1,4 +1,4 @@
-var media = "https://c.tenor.com/OZy3cP4bAvkAAAAS/revenge-ninja-peek-a-boo.gif";
-var memeImage = "https://c.tenor.com/OZy3cP4bAvkAAAAS/revenge-ninja-peek-a-boo.gif";
-var topText = "Top Text goes Here";
-var bottomText = "Bottom Text goes Here testing the overflow of longer text";
+var media = "";
+var memeImage = "";
+var topText = "";
+var bottomText = "";
